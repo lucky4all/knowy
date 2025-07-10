2a7ee7f9e58d01e45ca5491ed8f40ca36178dd6e
Lucky4all <lucasromerocoscia@gmail.com>
2025-07-10 05:18:20 -0300

    feat: added appwrite

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+
 const app = express();
+
 require('dotenv').config();
+
 const port = process.env.PORT;
+
+
 app.use(express.json());
 app.use(cors());
 app.use(helmet())
@@ -12,6 +17,40 @@ app.get("/", (req, res) => {
     res.json({ estado: "servidor funcionando" });
  }
 )
+
+/*
+
+Appwrite client SDK
+
+*/
+
+import { Client, Databases, ID } from "appwrite"
+
+const client = new Client();
+const db = new Databases(client);
+
+client
+    .setEndpoint(process.env.API_ENDPOINT)
+    .setProject(process.env.PROJECT_ID)
+    .setKey(process.env.API_KEY)
+;
+
+app.post("/preauth", async (req, res) => {
+    let response = req.body;
+    if (!response.name || !response.username || !response.email) {
+        res.json({ operation: "Faltan datos del cliente" }).status(400);
+        return;
+    }
+
+    try {
+        const res = await db.createDocument('6864a3ee00160df88c81', '686f72fb00007726ce23', ID.unique(), response);
+        res.json({ operation: "usuario creado con exito" }).status(201);
+    } catch(er) {
+        console.log(er);
+        res.json({ operation: "No se ha podido crear el usuario" }).status(500)
+    }
+    
+})
 
 app.listen(port, () => {
     console.log("Servidor corriendo port el puerto: ", port)
